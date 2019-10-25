5b13a0b96abfa8dd253956a7ef86a83f1fba62ec
oyelami hafeez <oyelami.abdulhafeez@gmail.com>
2019-10-25 17:49:01 +0100

    update

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,13 +3,24 @@ import './Chat.css';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+let socket;
+
 const Chat = ({location}) => {
+
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
     const {name, room} = queryString.parse(location.search);
     //console.log(location.search);
-    console.log(name, room);
-  })
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    console.log(socket);
+  }, [ENDPOINT, location.search ])
 
   return (
     <div>
